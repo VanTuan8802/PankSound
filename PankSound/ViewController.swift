4df1dc926658f4c8226e03d65253927756361cdc
VanTuan8802 <tuannv@vtn-global.com>
2024-04-02 16:11:13 +0700

    home vc

--- PankSound/ViewController.swift
+++ PankSound/ViewController.swift
@@ -6,14 +6,52 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVAudioRecorderDelegate {
 
+    @IBOutlet weak var recordBtn: UIButton!
+    @IBOutlet weak var recordLb: UILabel!
+    @IBOutlet weak var stopBtn: UIButton!
+    
+    var audioRecorder: AVAudioRecorder!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setUI()
+    }
+    
+    private func setUI() {
+        stopBtn.isEnabled = false
     }
 
+    @IBAction func recordAction(_ sender: Any) {
+        recordLb.text = "Recording in progress"
+        
+        stopBtn.isEnabled = true
+        recordBtn.isEnabled = false
+        
+        let dirPath = NSSearchPathForDirectoriesInDomains(.documentDirectory,.userDomainMask, true)[0] as String
+        let recordingName = UUID().uuidString
+        let pathArray = [dirPath, recordingName]
+        let filePath = URL(string: pathArray.joined(separator: "/"))
+        print(filePath)
 
+        let session = AVAudioSession.sharedInstance()
+        try! session.setCategory(AVAudioSession.Category.playAndRecord, mode: AVAudioSession.Mode.default, options: AVAudioSession.CategoryOptions.defaultToSpeaker)
+
+        try! audioRecorder = AVAudioRecorder(url: filePath!, settings: [:])
+        audioRecorder.delegate = self
+        audioRecorder.isMeteringEnabled = true
+        audioRecorder.prepareToRecord()
+        audioRecorder.record()
+    }
+    
+    @IBAction func stopAction(_ sender: Any) {
+        recordBtn.isEnabled = true
+        stopBtn.isEnabled = false
+        recordLb.text = "Tap to Record"
+    }
 }
 
